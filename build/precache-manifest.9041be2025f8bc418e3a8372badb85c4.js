0029fe82510cfbba02bdbc261069babb8370730a
App Academy Student <aa@Apps-Mac-mini-14.local>
2020-01-13 18:14:49 -0500

    add built

---
+++ build/precache-manifest.9041be2025f8bc418e3a8372badb85c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0100d6ff4ed046ab74d4ce849051c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83126886283082283b99",
+    "url": "/static/css/main.dcd66d03.chunk.css"
+  },
+  {
+    "revision": "81babfb679048822d464",
+    "url": "/static/js/2.f539695c.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.f539695c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83126886283082283b99",
+    "url": "/static/js/main.d9d62f05.chunk.js"
+  },
+  {
+    "revision": "f6589156eee051b9595a",
+    "url": "/static/js/runtime-main.a5b1dd0a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
