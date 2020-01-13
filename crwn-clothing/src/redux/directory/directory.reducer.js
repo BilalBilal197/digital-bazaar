d4d845b6d5b1f1dc978539f20164657cf199fa81
App Academy Student <aa@Apps-Mac-mini-14.local>
2020-01-13 15:26:47 -0500

    updating categories

--- crwn-clothing/src/redux/directory/directory.reducer.js
+++ crwn-clothing/src/redux/directory/directory.reducer.js
@@ -1,37 +1,66 @@
 const INITIAL_STATE = {
   sections: [
     {
-      title: 'hats',
+      title: 'action',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
-      title: 'jackets',
+      title: 'adventure',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
-      title: 'sneakers',
+      title: 'indie',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
-      title: 'womens',
+      title: 'racing',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
-      title: 'mens',
+      title: 'RPG',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens'
+    },
+    {
+      title: 'sports',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens'
+    },
+    {
+      title: 'strategy',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens'
+    },
+    {
+      title: 'casual',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens'
+    },
+    {
+      title: 'simulation',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
     }
+
   ]
 };
 
